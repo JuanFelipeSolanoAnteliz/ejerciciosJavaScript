3d188b16a72fa21f9003759dd619f6a8c171f238
JuanFelipeSolanoAnteliz <juanfesolant0906@gmail.com>
2024-10-02 14:57:40 -0500

    feat: :sparkles: find smallestNumber

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
-let names =prompt("ingrese nombres separados por espacios")
-let nameList = names.split(' ');
-const saludos = async(names)=>{
-    nameList.forEach(name => {
-        console.log(`!hola ${name}¡`);
+
+let numbers=prompt("enter number with ONLY one space betwen them")
+// let numbers = '1 5 17'
+let numberList = numbers.split(' ');
+console.log(numberList)
+
+const numbersIdentifier = async(numberList)=>{
+    let smallestNumber = parseInt(numberList[0])
+    numberList.forEach(number => {
+        let parsedNumber = parseInt(number)
+        if(parsedNumber < smallestNumber){
+            smallestNumber = number
+        }
     });
+    console.log(`the smallest number is: ${smallestNumber}`)
 }
-saludos()
+numbersIdentifier(numberList)
+
+
