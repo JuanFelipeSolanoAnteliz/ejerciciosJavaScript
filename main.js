d570d25c4ce96d3b3bcb1c0d998c07913fadbf6a
LuisFernandoSolanoB <fexijust8594@gmail.com>
2024-10-02 19:53:35 -0500

    feat: :sparkles: Frodo safe

--- main.js
+++ main.js
@@ -1,18 +1,34 @@
-let elements = 'que si cristian que si'
-let elementToFind = 'si'
+// Ingresar nombres: Sam Frodo Legolas
+// Sam y Frodo están juntos, ¡Frodo está a salvo!
 
-const findElement = async (elements, search) =>{
-    let elementsList = elements.split(' ');
-    console.log(elementsList)
-    let auth = false;
-    elementsList.forEach((element, index) => {
-        if(element === search){
-            console.log(`"${element}" is in the index number: ${index}`);
-            auth = true;
+// Ingresar nombres: Sam Orco Frodo
+// Sam y Frodo están separados, ¡Frodo está en peligro!
+const names = 'Sam pepe Orco Frodo'
+
+const findFrodo = async(names)=>{   
+    let namesLits = names.split(' ');
+    console.log(namesLits);
+    let frodoSafe = false; 
+    namesLits.forEach((element,index) => {
+        if(element == 'Sam' & index != 0 ){
+            console.log('hola, soy Sam',index)
+            if(namesLits[index+1] == 'Frodo' || namesLits[index-1] == 'Frodo'){
+                frodoSafe  = true;
+            }
+        }
+        else if( element == 'Sam' & index == 0){
+            console.log('sam en 0')
+            if(namesLits[index+1] == 'Frodo'){
+            frodoSafe  = true;
+            }
         }
     });
-    if( auth === false){
-        console.log(`${search} is not the words that were provided`);
+    if(!frodoSafe){
+        console.log('Frodo es alejado, cuidado')
     }
+    else if(frodoSafe){
+        console.log('Frodo esta a salvo')
+    } 
 }
-await findElement(elements,elementToFind);
+
+await findFrodo(names)
